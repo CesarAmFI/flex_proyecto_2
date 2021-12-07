8e135b37f65ecec656537a380b0614013d7c5e7d
CesarAmFI <90115835+CesarAmFI@users.noreply.github.com>
2021-12-07 14:07:28 -0600

    Add files via upload

---
+++ tokens.h
@@ -0,0 +1,46 @@
+#ifndef TOKENS_H
+#define TOKENS_H
+
+namespace C_1{ 
+
+    const int PYC = 1;
+    const int ID = 2; 
+    const int CHAR = 3;
+    const int INT = 4; 
+    const int FLOAT = 5;
+    const int DOUBLE = 6;
+    const int STRUCT = 7;
+    const int LLAVE_D = 8;
+    const int LLAVE_I = 9;
+    const int PAR_D = 10;
+    const int PAR_I = 11;
+    const int VOID = 12;
+    const int COMA = 13;
+    const int IF = 14;
+    const int ELSE = 15;
+    const int WHILE = 16;
+    const int DO = 17;
+    const int IGUAL = 18;
+    const int PUNTO = 19;
+    const int PRINT = 20;
+    const int SCAN = 21; 
+    const int BREAK = 22;
+    const int RETURN = 23;
+    const int MAS = 24;
+    const int MENOS = 25;
+    const int MULT = 26;
+    const int DIV = 27;
+    const int NUMERO = 28;
+    const int CADENA = 29;
+    const int OR = 30;
+    const int AND = 31;
+    const int MENOR = 32;
+    const int MAYOR = 33;
+    const int DIF = 34;
+    const int D_IGUAL = 35;
+    const int MAY_IG = 36;
+    const int MEN_IG = 37;
+    const int ESP = 38;
+}
+
+#endif
